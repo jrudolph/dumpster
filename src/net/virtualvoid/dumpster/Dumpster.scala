afa42929afe18ca867baaa1b0e1a6093ee632561
Johannes Rudolph <johannes_rudolph@gmx.de>
2008-09-03 18:40:20 +0200

    yeah: it is now possible to browse the current dir

--- src/net/virtualvoid/dumpster/Dumpster.scala
+++ src/net/virtualvoid/dumpster/Dumpster.scala
@@ -1,8 +1,69 @@
 package net.virtualvoid.dumpster
 
+import scala.xml.{Node,Elem}
+
+trait Resource {
+  def property(prop:Node):Option[Node] = prop match {
+  case t @ <resourcetype/> => Some(t)
+  case _ => None
+  }
+  def url:String
+  def children:Seq[Resource]
+}
+
+class FileResource(file:java.io.File) extends Resource{
+  val formatter = java.text.DateFormat.getDateInstance(java.text.DateFormat.LONG)
+  def gmt(time:long):String = formatter.format(new java.util.Date(time))
+  
+  override def property(prop:Node):Option[Node] = prop match{
+  case <getlastmodified/> => Some(<D:getlastmodified>{gmt(file.lastModified)}</D:getlastmodified>)
+  case <getcontentlength/> => Some(<D:getcontentlength>{file.length}</D:getcontentlength>)
+  case <resourcetype/> => {
+    System.out.println(file.getAbsolutePath+" "+file.isDirectory)
+    if (file.isDirectory) Some(<D:resourcetype><D:collection/></D:resourcetype>) else Some(<D:resourcetype/>)
+  }
+  case _ => super.property(prop)
+  }
+  def url = "http://localhost:7070"+file.getPath.substring(1)+(if (file.isDirectory) "/" else "")
+  def children = file.listFiles.map(new FileResource(_)) 
+}
+
 class MyHandler extends org.mortbay.jetty.handler.AbstractHandler{
   import javax.servlet.http._
   import org.mortbay.jetty.Request
+  import scala.xml._
+  
+  def propfind(props:NodeSeq,target:String,depth:String) = {
+    val file = new java.io.File("." + target)
+    //System.out.println(file.getAbsolutePath+" "+file.isDirectory)
+    val res:Resource = new FileResource(file)
+
+    val resources:Seq[Resource] = depth match {
+    case "0" => res::Nil
+    case "1" => res.children ++ (res :: Nil)
+    }
+    
+    <D:multistatus xmlns:D="DAV:">
+      {resources.map(res =>{
+        val mapped:Seq[(Node,Option[Node])] = props.map(p => (p,res.property(p)))
+      <D:response>
+        <D:href>{res.url}</D:href>
+        <D:propstat xmlns:D="DAV:">
+          <D:prop>
+            {mapped.flatMap(_ match {case (_,Some(p))=>p::Nil;case (_,None)=>Nil})}
+	      </D:prop>
+	      <D:status>HTTP/1.1 200 OK</D:status>
+        </D:propstat>
+        <D:propstat xmlns:D="DAV:">
+          <D:prop>
+            {mapped.flatMap(_ match {case (_,Some(p))=>Nil;case (p,None)=>p})}
+	      </D:prop>
+	      <D:status>HTTP/1.1 404 Not Found</D:status>
+        </D:propstat>
+      </D:response>})}
+    </D:multistatus>
+  }
+  
   def handle(target:String,req:HttpServletRequest,res:HttpServletResponse,dispatch:int){
     val r:Request = req.asInstanceOf[Request]
     r.setHandled(true)
@@ -15,36 +76,12 @@ class MyHandler extends org.mortbay.jetty.handler.AbstractHandler{
       res.setContentType("application/xml")
       res.setStatus(207,"Multi-Status")
       val depth = req.getHeader("Depth")
-      scala.xml.XML.write(res.getWriter,
-<D:multistatus xmlns:D="DAV:">
-  <D:response>
-    <D:href>http://localhost:7070/</D:href>
-    <D:propstat>
-      <D:prop>
-        <D:resourcetype><D:collection/></D:resourcetype>
-      </D:prop>
-      <D:status>HTTP/1.1 200 OK</D:status>
-    </D:propstat>
-  </D:response>
-  {if (depth == "1") (<D:response>
-    <D:href>http://localhost:7070/wurst</D:href>
-    <D:propstat>
-    <D:prop>
-      <D:resourcetype/>
-    </D:prop>
-    <D:status>HTTP/1.1 200 OK</D:status>
-    </D:propstat>
-  </D:response>
-  <D:response>
-    <D:href>http://localhost:7070/blub</D:href>
-    <D:propstat>
-      <D:prop>
-        <D:resourcetype/>
-      </D:prop>
-      <D:status>HTTP/1.1 200 OK</D:status>
-    </D:propstat>
-  </D:response>)}
- </D:multistatus>,"utf-8",true,null)
+      
+      import scala.xml.XML
+      
+      val input = XML.load(req.getInputStream)
+      
+      XML.write(res.getWriter,propfind(input \ "prop" \ "_" ,target,depth),"utf-8",true,null)
     }
     case _ =>
       r.setHandled(false)
